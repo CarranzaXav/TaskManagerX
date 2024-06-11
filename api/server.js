dc97f25f5c02241bb518df3080074e0ee7e821ce
CarranzaXav <carranzax7@gmail.com>
2024-06-10 22:17:11 -0400

    Update version 1.0.8

--- api/server.js
+++ api/server.js
@@ -3,13 +3,16 @@ const mysql = require("mysql");
 const cors = require("cors");
 const path = require("path");
 
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
