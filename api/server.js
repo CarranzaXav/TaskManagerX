f0a1d529715c93cd237b9fdd88a9b6b41234a884
CarranzaXav <carranzax7@gmail.com>
2024-06-10 22:25:28 -0400

    Update version 1.0.9

--- api/server.js
+++ api/server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
